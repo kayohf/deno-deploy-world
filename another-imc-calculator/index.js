61573e7d6f0ae5135ddc6d15b20278c5de27a7d6
Kayo Hamid <kayohamid@outlook.com>
2021-07-03 18:19:03 -0300

    fix syntax pt8 =(((

--- another-imc-calculator/index.js
+++ another-imc-calculator/index.js
@@ -1,26 +1,4 @@
 addEventListener("fetch", (event) => {
-
-    // if (event.request.method !== "GET") {
-    //     const response = new Response(null, { status: 405, statusText: "Method Not Allowed" });
-    //     return event.respondWith(response);
-    // }
-
-    // const urlParams = event.request.url.split('?');
-    // const params = new URLSearchParams(urlParams[1]);
-    // if (!params.has('peso') || !params.has('altura')) {
-    //     const response = new Response("Informe um peso e uma altura", {
-    //         status: 400,
-    //         statusText: "Invalid query params"
-    //     });
-
-    //     return event.respondWith(response);
-    // }
-
-    // let peso = parseFloat(params.get('peso').replace(',', '.'))
-    // let altura = parseFloat(params.get('altura').replace(',', '.'))
-
-    // let imc = peso / (altura * altura);
-
     const response = new Response('E agora jose, vai dar bug tambem?', {
         headers: {
             'cotent-type': 'text/plain'
