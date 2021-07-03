fd9e1a3ea17c8ffb97a1a18a2e0e52002ad7b8ef
Kayo Hamid <kayohamid@outlook.com>
2021-07-03 18:13:43 -0300

    fix syntax pt2 =)

--- another-imc-calculator/index.js
+++ another-imc-calculator/index.js
@@ -1,24 +1,24 @@
 addEventListener("fetch", (event) => {
 
     if (event.request.method !== "GET") {
-        return event.respondWith(
-            new Response(null, {
-                status: 405,
-                statusText: "Method Not Allowed"
-            })
-        );
+        const response = new Response(null, {
+            status: 405,
+            statusText: "Method Not Allowed"
+        });
+
+        return event.respondWith(response);
     }
 
     const urlParams = event.request.url.split('?');
     const params = new URLSearchParams(urlParams[1]);
 
     if (!params.has('peso') || !params.has('altura')) {
-        return event.respondWith(
-            new Response("Informe um peso e uma altura", {
-                status: 400,
-                statusText: "Invalid query params"
-            })
-        );
+        const response = new Response("Informe um peso e uma altura", {
+            status: 400,
+            statusText: "Invalid query params"
+        });
+
+        return event.respondWith(response);
     }
 
     let peso = parseFloat(params.get('peso').replace(',', '.'))
